882a4e63d58ae962014fdcd80c29a3a15fe9a0ad
Nitesh Gupta <niteshguptampb19@gmail.com>
2021-09-08 23:12:42 +0530

    add a controller for / route

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const home_controller = require('../controllers/home_controllers');
+
+
+router.get('/', home_controller.home);
 
 
 
