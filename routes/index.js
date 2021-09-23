485502c52bfa759b4078347a9abf435ea37ad856
Nitesh Gupta <niteshguptampb19@gmail.com>
2021-09-23 22:03:57 +0530

    set up the ejs

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const home_controller = require('../controllers/home_controllers');
 
+//set up view engine
+app.set('view engine', 'ejs');
+app.set('views', './views');
+
 
 router.get('/', home_controller.home);
 router.use('/users', require('./users'));
