a3aabe5c4c6c411fe03a1d759329676e5e79921b
Nitesh Gupta <niteshguptampb19@gmail.com>
2021-10-12 21:40:31 +0530

    manual authentication

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -1,5 +1,7 @@
 module.exports.home = function (req, res) {
+  console.log(req.cookies);
+  res.cookie('user_id', 101);
   return res.render('index', {
     "title": "Home",
-  })
+  });
 }
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,5 +1,47 @@
+const User = require('../models/user');
+
 module.exports.profile = function (req, res) {
   return res.render('profile', {
     "name": "Javascript",
+  });
+}
+
+module.exports.signUp = function (req, res) {
+  return res.render('user_sign_up', {
+    title: "Codial | Sign Up"
+  });
+}
+
+module.exports.signIn = function (req, res) {
+  return res.render('user_sign_in', {
+    title: "Codial | Sign In"
+  });
+}
+
+//get the signup data
+module.exports.create = function (req, res) {
+  console.log(req.body);
+  if (req.body.password != req.body.confirm_password) {
+    return res.redirect('back');
+  }
+
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) { console.log('error in finding user in signing up'); }
+    if (!user) {
+      User.create(req.body, function (err, user) {
+        if (err) {
+          console.log('Error in creating User in signing Up');
+          return;
+        }
+        return res.redirect('/users/sign-in');
+       })
+    } else {
+      return res.redirect('back');
+    }
   })
+}
+
+//sign in and create a session for user
+module.exports.createSession = function (req, res) {
+  // TODO Later
 }
