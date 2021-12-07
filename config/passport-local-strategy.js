facdc71cc08ff3ef20135a459d5bf8638bbde609
Nitesh Gupta <niteshguptampb19@gmail.com>
2021-12-07 22:26:45 +0530

    changes in passport-local file

---
+++ config/passport-local-strategy.js
@@ -0,0 +1,41 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').LocalStrategy;
+const User = require('../models/user');
+
+// Authentication using passport
+passport.use(new LocalStrategy({
+  UserNameField: 'email'
+},
+  function (email, password, done) {
+    // find the user and establish the identity
+    User.findOne({ email: email }, function (err, user) {
+      if (err) {
+        console.log('Error in finding user ----> Passport');
+        return done(err);
+      }
+      if (!user || user.password != password) {
+        console.log('Invalid Username/Passport');
+        return done(null, false);
+      }
+      return done(null, user);
+    })
+  }
+));
+
+// Serializing  the user to decide which key is to be kept in cookies
+passport.serializeUser(function (user, done) {
+  done(null, user.id);
+});
+
+// deserializing the user from the key in cookies
+passport.deserializeUser(function (id, done) {
+  User.findById(id, function (err, user) {
+    if (err) {
+      console.log('Error in finding user ----> Passport');
+      return done(err);
+    }
+    return done(null, user);
+  });
+});
+
+module.exports = passport;
